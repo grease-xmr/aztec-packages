1ae15afa46787881851a9a0288f55c76b2effc5a
CjS77 <CjS77@users.noreply.github.com>
2025-11-25 23:49:31 +0000

    chore: remove unused crypto functions
    Removes non-ZK functions (e.g. Schnorr signatures, ECDSA, hashing etc) from the
    bb_rs API. The only real interest for this library are the Snark provers
    and related utilities.
    
    Pure rust implementations for the removed functions already exist.

--- barretenberg/bb_rs/wrapper.hpp
+++ barretenberg/bb_rs/wrapper.hpp
@@ -1,11 +1,4 @@
-#include <barretenberg/crypto/pedersen_commitment/c_bind.hpp>
-#include <barretenberg/crypto/pedersen_hash/c_bind.hpp>
 #include <barretenberg/crypto/poseidon2/c_bind.hpp>
-#include <barretenberg/crypto/blake2s/c_bind.hpp>
-#include <barretenberg/crypto/aes128/c_bind.hpp>
-#include <barretenberg/crypto/schnorr/c_bind.hpp>
-#include <barretenberg/crypto/ecdsa/c_bind.h>
-#include <barretenberg/ecc/curves/secp256k1/c_bind.hpp>
 #include <barretenberg/srs/c_bind.hpp>
 #include <barretenberg/common/c_bind.hpp>
 #include <barretenberg/dsl/acir_proofs/c_bind.hpp>
@@ -18,7 +11,4 @@ extern "C" {
     void ecc_grumpkin__batch_mul(uint8_t const* point_buf, uint8_t const* scalar_buf, uint32_t num_points, uint8_t* result);
     void ecc_grumpkin__get_random_scalar_mod_circuit_modulus(uint8_t* result);
     void ecc_grumpkin__reduce512_buffer_mod_circuit_modulus(uint8_t* input, uint8_t* result);
-
-    // BN254 function declarations (no header file exists)
-    void bn254_fr_sqrt(uint8_t const* input, uint8_t* result);
 }

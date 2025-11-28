d22edcb4e16f58e8e33d28435021ee5e75321df6
CjS77 <CjS77@users.noreply.github.com>
2025-11-28 16:40:46 +0000

    chore: re-export boncode serialze
    It's used in the flow right now, so expose it rather than require
    another dependency.

--- barretenberg/zkuh-rs/src/noir_api/mod.rs
+++ barretenberg/zkuh-rs/src/noir_api/mod.rs
@@ -7,6 +7,6 @@ pub use api::{compile, execute, CompilationResult, ExecutionResult, NoirError};
 pub use inputs::{FieldInput, InputError, Inputs, PointInput};
 
 // re-export
-pub use acir::circuit::Program;
+pub use acir::{circuit::Program, bincode_deserialize, bincode_serialize};
 pub use noirc_artifacts::program::ProgramArtifact;
 pub use noirc_driver::CompileOptions;

8cc98429affb07c3498d4bdd99eead37713e2d97
CjS77 <CjS77@users.noreply.github.com>
2026-01-06 21:52:04 +0000

    fix: expose public struct
    RunnerResult should be exposed as part of the public API. This fixes
    that.
    
    Bumps patch to v1.10.1

--- barretenberg/zkuh-rs/src/runner/mod.rs
+++ barretenberg/zkuh-rs/src/runner/mod.rs
@@ -4,4 +4,4 @@ mod runner;
 pub use bytecode_verification::{
     ByteCodeVerification, DummyByteCodeVerifier, HashByteCodeVerifier,
 };
-pub use runner::{BytecodeError, ExecutionError, ProofRunner, VerificationRunner};
+pub use runner::{BytecodeError, ExecutionError, ProofRunner, VerificationRunner, RunnerResult};
